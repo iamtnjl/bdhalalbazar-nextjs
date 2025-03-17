ce58da7f640bd4825593c61860592d2bd0954731
tanjil-repliq <md.tanjil@repliq.dev>
2025-03-17 16:14:36 +0600

    Update font style

--- components/shared/TopNavBar.jsx
+++ components/shared/TopNavBar.jsx
@@ -6,7 +6,7 @@ const TopNavbar = () => {
     <nav className="px-4 py-3 bg-white  shadow-sm flex items-center justify-between sticky top-0 z-50 rounded-bl-md rounded-br-md">
       <div className="flex gap-1 items-center">
         <Image alt="logo" src={"/logo/logo.png"} width={50} height={50} />
-        <p className="bg-gradient-to-tr from-primary-700 to-cyan-600 bg-clip-text text-transparent text-3xl font-semibold font-lansui">
+        <p className="bg-gradient-to-tr from-primary-700 to-cyan-600 bg-clip-text text-transparent text-3xl font-semibold iansui-regular">
           BDHalalBazar
         </p>
       </div>
