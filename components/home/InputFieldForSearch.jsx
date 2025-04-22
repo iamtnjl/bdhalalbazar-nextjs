6567cc98ad171c0cd5f6ab97810bea953a71c3d9
iamtnjl <md.tanjil@repliq.dev>
2025-04-22 16:39:21 +0600

    Design tweaks

--- components/home/InputFieldForSearch.jsx
+++ components/home/InputFieldForSearch.jsx
@@ -5,10 +5,11 @@ import { Search } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const InputFieldForSearch = () => {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
   const router = useRouter();
+
   return (
-    <div className=" w-full flex items-center gap-4 mt-2 px-2">
+    <div className="w-full flex items-center gap-4 mt-2 px-2">
       <div className="relative w-full">
         <Search
           className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-700"
@@ -17,17 +18,17 @@ const InputFieldForSearch = () => {
         <input
           type="text"
           placeholder="Search any products"
-          className="w-full pl-11 pr-5 py-[9px] rounded-full border-2 border-primary text-base text-gray-700 font-semibold focus:outline-none "
+          className="w-full pl-11 pr-12 py-[9px] rounded-full border-2 border-primary text-base text-gray-700 font-semibold focus:outline-none"
           onChange={(e) => setValue(e.target.value)}
         />
-        <div
+        <button
           onClick={() => {
             router.push(`/products?search=${value}`);
           }}
-          className="p-[11px] rounded-full bg-primary w-fit right-[2px] absolute top-[2px]"
+          className="absolute top-1/2 -translate-y-1/2 right-2 p-2 bg-primary rounded-full"
         >
           <Search className="text-white" size={20} />
-        </div>
+        </button>
       </div>
     </div>
   );
