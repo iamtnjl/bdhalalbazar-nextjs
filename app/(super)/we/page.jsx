84aff4790e376c7471f4c7563acf796b6e7f7865
iamtnjl <md.tanjil@repliq.dev>
2025-05-19 15:39:35 +0600

    Updated dashboard stats

--- app/(super)/we/page.jsx
+++ app/(super)/we/page.jsx
@@ -17,42 +17,84 @@ const Dashboard = () => {
       <SectionTitle title="Dashboard" />
       <div className="grid grid-cols-2 gap-3">
         <div className="p-4 rounded-md border border-gray-200 bg-white">
-          <p className="text-base font-medium text-gray-500">Order Placed</p>
+          <p className="text-base font-medium text-gray-500">Gross Profit</p>
           <p className="text-2xl font-bold text-gray-700">
-            {data?.totalOrdersPlaced}
+            {Number(data?.orders?.grossProfit).toFixed(2)}
           </p>
         </div>
         <div className="p-4 rounded-md border border-gray-200 bg-white">
-          <p className="text-base font-medium text-gray-500">Pending Orders</p>
+          <p className="text-base font-medium text-gray-500">New users</p>
           <p className="text-2xl font-bold text-gray-700">
-            {data?.totalPendingOrders}
+            {data?.users?.activeCreatedInRange}
           </p>
         </div>
-        <div className="p-4 rounded-md border border-gray-200 bg-white">
-          <p className="text-base font-medium text-gray-500">
-            Completed Orders
-          </p>
-          <p className="text-2xl font-bold text-gray-700">
-            {data?.totalCompletedOrders}
-          </p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-xl font-semibold text-gray-700">Products summary</p>
+        <div className="grid grid-cols-2 gap-3">
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">
+              Total products
+            </p>
+            <p className="text-2xl font-bold text-gray-700">
+              {data?.products?.total}
+            </p>
+          </div>
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">Updated today</p>
+            <p className="text-2xl font-bold text-gray-700">
+              {data?.products?.updatedInRange}
+            </p>
+          </div>
         </div>
-        <div className="p-4 rounded-md border border-gray-200 bg-white">
-          <p className="text-base font-medium text-gray-500">Order Amount</p>
-          <p className="text-2xl font-bold text-gray-700">
-            {Number(data?.totalOrderAmount).toFixed(2)}
-          </p>
-        </div>
-        <div className="p-4 rounded-md border border-gray-200 bg-white">
-          <p className="text-base font-medium text-gray-500">Purchase Amount</p>
-          <p className="text-2xl font-bold text-gray-700">
-            {Number(data?.totalPurchaseAmount).toFixed(2)}
-          </p>
-        </div>
-        <div className="p-4 rounded-md border border-gray-200 bg-white">
-          <p className="text-base font-medium text-gray-500">User Created</p>
-          <p className="text-2xl font-bold text-gray-700">
-            {data?.totalUserCreated}
-          </p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-xl font-semibold text-gray-700">Orders summary</p>
+        <div className="grid grid-cols-2 gap-3">
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">Order Placed</p>
+            <p className="text-2xl font-bold text-gray-700">
+              {data?.orders?.total}
+            </p>
+          </div>
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">
+              Pending Orders
+            </p>
+            <p className="text-2xl font-bold text-gray-700">
+              {data?.orders?.totalPendingOrders}
+            </p>
+          </div>
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">
+              Completed Orders
+            </p>
+            <p className="text-2xl font-bold text-gray-700">
+              {data?.orders?.totalCompletedOrders}
+            </p>
+          </div>
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">
+              Canceled Orders
+            </p>
+            <p className="text-2xl font-bold text-gray-700">
+              {data?.orders?.canceledCount}
+            </p>
+          </div>
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">Order Amount</p>
+            <p className="text-2xl font-bold text-gray-700">
+              {Number(data?.orders?.totalAmount).toFixed(2)}
+            </p>
+          </div>
+          <div className="p-4 rounded-md border border-gray-200 bg-white">
+            <p className="text-base font-medium text-gray-500">
+              Purchase Amount
+            </p>
+            <p className="text-2xl font-bold text-gray-700">
+              {Number(data?.orders?.totalPurchaseAmount).toFixed(2)}
+            </p>
+          </div>
         </div>
       </div>
     </div>
