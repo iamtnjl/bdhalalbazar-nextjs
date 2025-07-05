67ce67b25e6b447d177ad4d063625cd936931164
iamtnjl <md.tanjil@repliq.dev>
2025-07-06 01:58:55 +0600

    Added apps info

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ import TopLoader from "@/components/shared/TopLoader";
 import Script from "next/script";
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import '@/i18n'; 
+import "@/i18n";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -48,6 +48,32 @@ export default function RootLayout({ children }) {
         <meta
           name="keywords"
           content="pabna grocery delivery, online grocery store pabna, BDHalalBazar, halal groceries pabna, fresh meat pabna, fruits and vegetables delivery, pabna food delivery, online halal shop Bangladesh, daily essentials pabna, halal food pabna, online grocery shop in pabna, grocery shop near me, halal online shopping, same-day delivery groceries, online grocery shop pabna, online bazar pabna, online bazar delivery pabna"
+        />
+
+        <Script
+          id="facebook-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '${process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v19.0'
+        });
+        FB.AppEvents.logPageView();   
+      };
+
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    `,
+          }}
         />
 
         {/* Meta Pixel */}
